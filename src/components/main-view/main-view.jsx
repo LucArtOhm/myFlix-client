c15c33b39818bc84ce4d81a296d88e91fc163edc
LucArtOhm <90980974+LucArtOhm@users.noreply.github.com>
2021-11-29 14:14:48 +0100

    Create main-view component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import MainView from './components/main-view/main-view';
-import './index.scss';
 
-class MyFlixApplication extends React.Component {
+export class MainView extends React.Component {
+
   render() {
     return (
-      <MainView />
+      <div className='main-view'>
+        <div>Amistad</div>
+        <div>Maudie</div>
+        <div>100 Metros</div>
+      </div>
     );
   }
 }
-
-const container = document.getElementsByClassName('app-container')[0];
-ReactDOM.render(React.createElement(MyFlixApplication), container);
