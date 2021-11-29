2b06302492aafb97353042412662d27fae3fff65
LucArtOhm <90980974+LucArtOhm@users.noreply.github.com>
2021-11-29 11:39:41 +0100

    Create components and main-view folders

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+
+  render() {
+    return (
+      <div className='main-view'>
+        <div>Amistad</div>
+        <div>Maudie</div>
+        <div>100 Metros</div>
+      </div>
+    );
+  }
+}
