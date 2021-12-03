1fdca6da3ab4327eef4f392e45c90514625900e2
LucArtOhm <90980974+LucArtOhm@users.noreply.github.com>
2021-12-03 12:33:04 +0100

    Remove list is empty message

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+
 
 export class MainView extends React.Component {
   constructor() {
@@ -15,6 +18,18 @@ export class MainView extends React.Component {
     };
   }
 
+  componentDidMount() {
+    axios.get('https://movies-your-favorites.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
@@ -24,14 +39,14 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
 
-    if (movies.length === 0) return <div className='main-view'>The list is empty!</div>;
+    if (movies.length === 0) return <div className='main-view' />;
 
     return (
       <div className='main-view'>
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
           ))
         }
       </div>
